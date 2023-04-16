d7dc8642b9ea3fed9424a6b3ecd7e6371a6641c0
Nhat Quang <tommyquanglowkey2011@gmail.com>
2023-04-16 10:45:00 +0700

    Add functions.py, the purpose is to have some useful functions for end-users

---
+++ functions.py
@@ -0,0 +1,10 @@
+"""
+    The purpose of this file is to provide some useful tools for end-users
+    Including the function for them to:
+        Choose the model (VGG_, Alexnet, Resnet, etc(VGG is recommended))
+        Change the hyper params
+        Choose image online
+        Upload image from their local machine
+        etc...
+    Also apply segmentation to choose region to apply transfer (if possible)
+"""
